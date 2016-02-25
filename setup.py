357a9e82713669302c2cc76fc994647bd7e879fb
intohole <intoblack86@gmail.com>
2016-02-25 10:51:50 +0800

    添加setup文件

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+kw = dict(
+    name='pyct',
+    version='0.0.1',
+    description='python simple crontab',
+    author='intohole',
+    author_email='intoblack86@gmail.com',
+    url='https://github.com/intohole/pyct',
+    download_url='https://github.com/intohole/pyct',
+    platforms='all platform',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True
+)
+
+setup(**kw)
